8b67fd87d7f7df5456a31c153a854085ee996ef0
yijiaduke <143843411+yijiaduke@users.noreply.github.com>
2024-10-05 20:53:40 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,19 @@
+"""
+ETL-Query script
+"""
+
+from mylib.extract import extract
+from mylib.transform_load import load
+from mylib.query import query
+
+# Extract
+print("Extracting data...")
+extract()
+
+# Transform and load
+print("Transforming data...")
+load()
+
+# Query
+print("Querying data...")
+query()
